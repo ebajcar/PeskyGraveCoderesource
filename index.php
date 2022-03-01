23654b37d211c58fbc8047bb0e5124df89b35793
Ellen Bajcar <ellen.bajcar@sheridaninstitute.ca>
2022-03-01 15:40:24 +0000

    added set 6

--- index.php
+++ index.php
@@ -5,5 +5,12 @@
   <body>
     <?php echo '<p>PHP examples</p>'; ?> 
     <a href="set1/example1.php">example 1</a>
+
+    <p>Set 6</p>
+    <a href="set6/hello_world.php">hello_world.php</a> | 
+    <a href="set6/print_printf.html">print_printf.html</a>  | 
+    <a href="set6/say_hello.php">say_hello.php</a> | 
+    <a href="set6/try_catch.php">try_catch.php</a> | 
+    <a href="set6/variables.php">variables.php</a>
   </body>
 </html>
