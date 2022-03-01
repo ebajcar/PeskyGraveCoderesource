6271229c3af246227f1b04c6196885864700fec5
Ellen Bajcar <ellen.bajcar@sheridaninstitute.ca>
2022-03-01 15:04:53 +0000

    added set1 folder

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+    <?php echo '<p>PHP examples</p>'; ?> 
+    <a href="set1/example1.php">example 1</a>
   </body>
 </html>
